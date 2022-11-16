f6ae37826a753cc7a3a242d29f36240723a570bf
vishnusasi7559 <vishnumkuttiady982@gmail.com>
2022-11-16 17:21:05 +0530

    simple list

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("#new-item-form");
+const list = document.querySelector("#list");
+const input = document.querySelector("#item-input");
+
+form.addEventListener('submit',e =>{
+    e.preventDefault();
+    const item =document.createElement('div');
+    item.innerHTML=input.value;
+    item.classList.add('list-item');
+    list.appendChild(item);
+    input.value ="";
+
+    item.addEventListener('click', e =>{
+        list.removeChild(item);
+    })
+})
